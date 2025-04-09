3fb7f06dabad536d038c03e2dda2c77db381c777
supriyo <hisupriyooo@gmail.com>
2025-04-10 00:24:54 +0530

    feat: better test utilities

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -366,6 +366,34 @@ func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
 
 	return true
 }
+func testLiteralExpression(t *testing.T, exp ast.Expression, expected interface{}) bool {
+	switch v := expected.(type) {
+	case int:
+		return testIntegerLiteral(t, exp, int64(v))
+	case int64:
+		return testIntegerLiteral(t, exp, v)
+	case string:
+		return testIdentifier(t, exp, v)
+	}
+	return false
+}
+func testInfixExpression(t *testing.T, exp ast.Expression, left interface{}, operator string, right interface{}) bool {
+
+	opExp, ok := exp.(*ast.InfixExpression)
+	if !ok {
+		t.Errorf("exp is not a ast.Operator expression. got=%T(%s)", exp, exp)
+	}
+	if !testLiteralExpression(t, opExp.Left, left) {
+		return false
+	}
+	if opExp.Operator != operator {
+		t.Errorf("exp.operator is not %s. got=%q", operator, opExp.Operator)
+	}
+	if !testLiteralExpression(t, opExp.Right, right) {
+		return false
+	}
+	return true
+}
 
 func checkParserErrors(t *testing.T, p *Parser) {
 	errors := p.Errors()
