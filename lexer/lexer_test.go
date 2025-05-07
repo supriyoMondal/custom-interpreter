c3e5d12017bcab2ccbdcce0df8236157ec30eb3a
supriyo <hisupriyooo@gmail.com>
2025-05-07 23:40:43 +0530

    feat: add support for raw string

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -26,6 +26,8 @@ return false;
 
 10 == 10;
 10 != 9;
+"foobar"
+"foo bar"
 `
 
 	tests := []struct {
@@ -105,6 +107,8 @@ return false;
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
 		{token.EOF, ""},
 	}
 
